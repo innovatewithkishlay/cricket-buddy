ad57764c0f8b8bba25c0914c2e32dea16c2d6ba5
kishlay <kishlay141@gmail.com>
2025-06-11 22:53:13 +0530

    started with the firebase

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,4 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, getReactNativePersistence } from "firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { getFirestore } from "firebase/firestore";
