4b90ed71c056f3ba1e293937c4b30f016491bf87
kishlay <kishlay141@gmail.com>
2025-06-12 13:39:36 +0530

    imported icons buttons and accesorires

--- src/screens/AuthScreen.tsx
+++ src/screens/AuthScreen.tsx
@@ -1,1 +1,11 @@
 import React, { useState, useEffect } from "react";
+import { View, StyleSheet, KeyboardAvoidingView, Platform } from "react-native";
+import {
+  Text,
+  Button,
+  TextInput,
+  Snackbar,
+  ActivityIndicator,
+  useTheme,
+} from "react-native-paper";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
