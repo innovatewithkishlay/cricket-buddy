712a9bf24e8bc1a00d274dc00e551c8c679ff377
kishlay <kishlay141@gmail.com>
2025-06-12 13:43:36 +0530

    updated the AppNavigator

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,9 +1,10 @@
 import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import AuthScreen from "../screens/AuthScreen";
 import HomeScreen from "../screens/HomeScreen";
 
 export type RootStackParamList = {
-  SignIn: undefined;
-  SignUp: undefined;
+  Auth: undefined;
   Home: undefined;
 };
 
@@ -11,11 +12,19 @@ const Stack = createStackNavigator<RootStackParamList>();
 
 export default function AppNavigator() {
   return (
-    <Stack.Navigator
-      initialRouteName="SignIn"
-      screenOptions={{ headerShown: false }}
-    >
-      <Stack.Screen name="Home" component={HomeScreen} />
-    </Stack.Navigator>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Auth">
+        <Stack.Screen
+          name="Auth"
+          component={AuthScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Cricket Buddy" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
