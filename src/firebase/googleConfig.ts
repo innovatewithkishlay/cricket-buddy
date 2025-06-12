d1f2ec9f656d21241a5fe73e341e07ea2fee9362
kishlay <kishlay141@gmail.com>
2025-06-12 13:27:12 +0530

    modified the googleConfig.ts

---
+++ src/firebase/googleConfig.ts
@@ -0,0 +1,6 @@
+export const ANDROID_CLIENT_ID =
+  "650052070673-0re7rkuupai1pbih40bl06k41gp050l.apps.googleusercontent.com";
+export const IOS_CLIENT_ID =
+  "650052070673-hk42vu41csl72bj5jpoluulta0m5p9iv.apps.googleusercontent.com";
+export const EXPO_CLIENT_ID =
+  "650052070673-uea19r9go721bebtoj63j1ipclo9s649.apps.googleusercontent.com";
