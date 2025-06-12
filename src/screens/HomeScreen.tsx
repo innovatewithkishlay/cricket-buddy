3799fbecc4e4a071b45766e07204c625c1bcb4d8
kishlay <kishlay141@gmail.com>
2025-06-12 09:42:18 +0530

    made basic layout of home for now

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,3 +1,17 @@
 import { Button } from "react-native-paper";
 import { View, StyleSheet } from "react-native";
 import { auth } from "../firebase/firebase";
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Button mode="contained" onPress={() => auth.signOut()}>
+        Sign Out
+      </Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: "center", alignItems: "center" },
+});
