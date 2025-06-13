1fe886046d87279d47baf06a14f49d307d918367
kishlay <kishlay141@gmail.com>
2025-06-13 13:03:51 +0530

    started working with the Loadertsx

---
+++ src/components/Loader.tsx
@@ -0,0 +1,4 @@
+import React, { useRef, useEffect } from "react";
+import { Animated, StyleSheet, View } from "react-native";
+import { BlurView } from "expo-blur";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
