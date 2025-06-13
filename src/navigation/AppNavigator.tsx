ee1b0a573bcdb92e421272e9c88ee9abcad42d0a
kishlay <kishlay141@gmail.com>
2025-06-13 13:59:51 +0530

    updated the AppNavigator and going to make two new files

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -4,11 +4,17 @@ import { createStackNavigator } from "@react-navigation/stack";
 import AuthScreen from "../screens/AuthScreen";
 import HomeScreen from "../screens/HomeScreen";
 import VerificationScreen from "../screens/VerificationScreen";
+import NewMatchScreen from "../screens/NewMatchScreen";
+import PastMatchesScreen from "../screens/PastMatchesScreen";
+import ExportScreen from "../screens/ExportScreen";
 
 export type RootStackParamList = {
   Auth: undefined;
   Home: undefined;
   Verification: { email: string };
+  NewMatch: undefined;
+  PastMatches: undefined;
+  Export: undefined;
 };
 
 const Stack = createStackNavigator<RootStackParamList>();
@@ -25,12 +31,27 @@ export default function AppNavigator() {
         <Stack.Screen
           name="Home"
           component={HomeScreen}
-          options={{ title: "Cricket Buddy" }}
+          options={{ title: "Cricket Buddy", headerShown: false }}
         />
         <Stack.Screen
           name="Verification"
           component={VerificationScreen}
           options={{ title: "Verify Email", headerShown: false }}
+        />
+        <Stack.Screen
+          name="NewMatch"
+          component={NewMatchScreen}
+          options={{ title: "New Match" }}
+        />
+        <Stack.Screen
+          name="PastMatches"
+          component={PastMatchesScreen}
+          options={{ title: "Past Matches" }}
+        />
+        <Stack.Screen
+          name="Export"
+          component={ExportScreen}
+          options={{ title: "Export Data" }}
         />
       </Stack.Navigator>
     </NavigationContainer>
