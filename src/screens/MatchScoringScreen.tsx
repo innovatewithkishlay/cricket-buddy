d076b9c8436432249546dea70c26f49dcfe6fe72
kishlay <kishlay141@gmail.com>
2025-06-14 09:53:37 +0530

    started with MatchScoringScreen

---
+++ src/screens/MatchScoringScreen.tsx
@@ -0,0 +1,5 @@
+import React, { useState, useEffect } from "react";
+import { View, ScrollView, StyleSheet } from "react-native";
+import { Text, Button, Card, TextInput, Divider } from "react-native-paper";
+import { RouteProp } from "@react-navigation/native";
+import { RootStackParamList } from "../navigation/AppNavigator";
